3a0e62475f2110202779341f1e660cebf9ffa61e
ahmedzidan <ahmedzidan_2015@hotmail.com>
2016-03-23 17:02:49 +0200

    Update class.signup.php

--- include/class.signup.php
+++ include/class.signup.php
@@ -19,3 +19,39 @@ if($_REQUEST['method'] == "insert" AND $_REQUEST['Fname'] AND $_REQUEST['Lname']
 	
 	
 }
+//function to call service ==
+function CallAPI($method, $url, $data = false) {
+    $curl = curl_init();
+    switch ($method) {
+        case "POST":
+            curl_setopt($curl, CURLOPT_POST, 1);
+
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+        case "PUT":
+            curl_setopt($curl, CURLOPT_PUT, 1);
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+        default:
+            if ($data)
+                $url = sprintf("%s?%s", $url, http_build_query($data));
+    }
+
+    // Optional Authentication:
+    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    $result = curl_exec($curl);
+
+    curl_close($curl);
+
+    return $result;
+}
+//===========================================================
+//=how to call service -------------
+CallAPI("GET", OPERATIONS_SERVICES_BASE_URL . "drivers/{$driver_id}");
